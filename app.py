9b29a0c3ed06b57a31fc398e74f36e2da9375f4b
Vasu Anumula <vanumula@gfsc.gg>
2026-02-27 11:51:33 +0000

    Few changes to file name

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello Vasu from FastAPI on Azure!"}
+    return {"message": "Hello from FastAPI running on Azure Python 3.14!"}

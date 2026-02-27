3bb5c7d191fe0ecf89e52a9b3904262cc93e84cc
Vasu Anumula <vanumula@gfsc.gg>
2026-02-27 11:27:10 +0000

    Made few changes to code

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Vasu! Welcome to Azure web apps"
-
-if __name__ == "__main__":
-    app.run()
+    return "Hello world from Azure!"

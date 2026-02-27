310b24eb5a12da844ea9779a6edea39c65c4caea
VasuAnumula <115710926+VasuAnumula@users.noreply.github.com>
2026-02-27 10:48:08 +0000

    Convert script to Flask web application

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello Vasu! Welcome to Azure web apps")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello Vasu! Welcome to Azure web apps"
+
+if __name__ == "__main__":
+    app.run()
